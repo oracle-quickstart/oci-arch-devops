73406a98b672df984195db126ba30ae817771b24
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-06-18 12:57:19 +0200

    Second commit

---
+++ devops_function/provider.tf
@@ -0,0 +1,24 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  region           = var.region
+  fingerprint      = var.fingerprint
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+}
+
+provider "oci" {
+  alias                = "homeregion"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  disable_auto_retries = "true"
+}
