f0062edcaf7609d9950acf22fbbe5aa5b842f91b
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-07-08 13:50:56 +0200

    Updated provider and variables

--- devops_instance_group/providers.tf
+++ devops_instance_group/providers.tf
@@ -27,9 +27,9 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
 
-  #  user_ocid        = var.user_ocid
-  #  fingerprint      = var.fingerprint
-  #  private_key_path = var.private_key_path
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
 }
 
 provider "oci" {
@@ -37,9 +37,9 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
 
-  #  user_ocid        = var.user_ocid
-  #  fingerprint      = var.fingerprint
-  #  private_key_path = var.private_key_path
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
 }
 
 provider "oci" {
@@ -47,7 +47,7 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
 
-  #  user_ocid        = var.user_ocid
-  #  fingerprint      = var.fingerprint
-  #  private_key_path = var.private_key_path
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
 }
--- devops_instance_group/variables.tf
+++ devops_instance_group/variables.tf
@@ -3,9 +3,9 @@
 
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
-#variable "user_ocid" {}
-#variable "fingerprint" {}
-#variable "private_key_path" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
 variable "region" {}
 variable "ssh_public_key" {
   default = ""
